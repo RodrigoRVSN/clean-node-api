c65b1a9475f0eda2bc1fd1ddb2819ec1e01f2ad2
rodrigorvsn <rodrigovictor81@gmail.com>
2023-06-18 16:15:32 -0300

    feat: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/utils/email-validator-adapter.ts
@@ -0,0 +1,7 @@
+import { type EmailValidator } from '../presentations/protocols/email-validator'
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid (email: string): boolean {
+    return false
+  }
+}
