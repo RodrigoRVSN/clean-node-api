0da557f00f284f85d0ea6bdfa34c5dc9da63666d
rodrigorvsn <rodrigovictor81@gmail.com>
2023-06-21 00:11:54 -0300

    feat: ensure AccountMongoRepository is returning accountData with the insertedId in mongodb

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -4,9 +4,7 @@ module.exports = {
       version: '4.0.3',
       skipMD5: true
     },
-    autoStart: false,
-    instance: {
-      dbName: 'jest'
-    }
+    instance: {},
+    autoStart: false
   }
 }
