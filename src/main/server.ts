261c6d3308030623ca1f340a5848a01c56c17362
rodrigorvsn <rodrigovictor81@gmail.com>
2023-06-22 23:01:16 -0300

    feat: connect mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,10 @@
-import app from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import { env } from './config/env'
 
-app.listen(5050, () => { console.log('🔥 server is running') })
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+
+    app.listen(5050, () => { console.log(`🔥 server is running in port ${env.port}`) })
+  })
+  .catch(console.error)
