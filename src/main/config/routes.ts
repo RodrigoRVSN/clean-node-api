e1deed87ce320ea128dd9e7dbaffde4837fcdfa4
rodrigorvsn <rodrigovictor81@gmail.com>
2023-06-22 22:45:21 -0300

    feat: ensure SignUp route returns an account on success

---
+++ src/main/config/routes.ts
@@ -0,0 +1,11 @@
+
+import { Router, type Express } from 'express'
+import fg from 'fast-glob'
+
+export const setupRoutes = (app: Express): void => {
+  const router = Router()
+  app.use('/api', router)
+  fg.sync('**/src/main/routes/**routes.ts').map(async file => {
+    (await import(`../../../${file}`)).default(router)
+  })
+}
