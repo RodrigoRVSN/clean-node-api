a82a622c6eba943214e02e59fc78c93f847f9d41
rodrigorvsn <rodrigovictor81@gmail.com>
2023-06-27 20:35:26 -0300

    feat: add Login factory

--- src/data/protocols/db/account/load-account-by-email-repository.ts
+++ src/data/protocols/db/account/load-account-by-email-repository.ts
@@ -1,4 +1,4 @@
-import { type AccountModel } from '../../usecases/add-account/db-add-account-protocols'
+import { type AccountModel } from '../../../usecases/add-account/db-add-account-protocols'
 
 export type LoadAccountByEmailRepository = {
   loadByEmail: (email: string) => Promise<AccountModel | null>
---
+++ src/main/factories/login/login-factory.ts
@@ -0,0 +1,24 @@
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
+import { LoginController } from '../../../presentations/controllers/login/login-controller'
+import { type Controller } from '../../../presentations/protocols'
+import { LogControllerDecorator } from '../../decorators/log/log-controller-decorator'
+import { makeLoginValidation } from './login-validation-factory'
+
+export const makeLoginController = (): Controller => {
+  const salt = 12
+
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const accountMongoRepository = new AccountMongoRepository()
+
+  const dbAddAccount = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+
+  const loginController = new LoginController(makeLoginValidation(), dbAddAccount)
+  const logMongoRepository = new LogMongoRepository()
+
+  return new LogControllerDecorator(loginController, logMongoRepository)
+}
