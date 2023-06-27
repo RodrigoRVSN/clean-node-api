e64038cced38c319e55534bce191fd146d3554f7
rodrigorvsn <rodrigovictor81@gmail.com>
2023-06-26 23:07:13 -0300

    refactor: move repository functions to data db protocols

--- src/data/protocols/add-account-repository.ts
+++
@@ -1,6 +0,0 @@
-import { type AccountModel } from '../../domain/models/account'
-import { type AddAccountModel } from '../../domain/usecases/add-account'
-
-export type AddAccountRepository = {
-  add: (accountData: AddAccountModel) => Promise<AccountModel>
-}
