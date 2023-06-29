cc0430528cad4311467d85a0ec80bf96bc902882
rodrigorvsn <rodrigovictor81@gmail.com>
2023-06-28 21:48:03 -0300

    feat: ensure returns 204 in succes and 500 when AddSurveyUseCase is called

--- src/presentations/controllers/survey/add-survey-controller.spec.ts
+++ src/presentations/controllers/survey/add-survey-controller.spec.ts
@@ -1,6 +1,6 @@
 import { type AddSurvey, type AddSurveyModel, type HttpRequest, type Validation } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
-import { badRequest } from '../../helpers/http/http-helpers'
+import { badRequest, noContent, serverError } from '../../helpers/http/http-helpers'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -70,5 +70,30 @@ describe('AddSurvey', () => {
 
     await sut.handle(makeFakeRequest())
     expect(validateSpy).toHaveBeenCalledWith(makeFakeRequest().body)
+  })
+
+  it('should call AddSurvey with correct values', async () => {
+    const { sut, addSurveyStub } = makeSut()
+
+    const validateSpy = jest.spyOn(addSurveyStub, 'add')
+
+    await sut.handle(makeFakeRequest())
+    expect(validateSpy).toHaveBeenCalledWith(makeFakeRequest().body)
+  })
+
+  it('should return 500 if AddSurvey throws', async () => {
+    const { sut, addSurveyStub } = makeSut()
+
+    jest.spyOn(addSurveyStub, 'add').mockReturnValue(new Promise((resolve, reject) => { reject(new Error()) }))
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  it('should return 204 on success', async () => {
+    const { sut } = makeSut()
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(noContent())
   })
 })
--- src/presentations/controllers/survey/add-survey-controller.ts
+++ src/presentations/controllers/survey/add-survey-controller.ts
@@ -1,4 +1,4 @@
-import { badRequest, ok } from '../../helpers/http/http-helpers'
+import { badRequest, noContent, serverError } from '../../helpers/http/http-helpers'
 import { type Validation, type Controller, type HttpRequest, type HttpResponse, type AddSurvey } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
@@ -8,13 +8,17 @@ export class AddSurveyController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) return badRequest(error)
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) return badRequest(error)
 
-    const { question, answers } = httpRequest.body
+      const { question, answers } = httpRequest.body
 
-    await this.addSurvey.add({ question, answers })
+      await this.addSurvey.add({ question, answers })
 
-    return await new Promise(resolve => { resolve(ok('')) })
+      return noContent()
+    } catch (error) {
+      return serverError(error as Error)
+    }
   }
 }
