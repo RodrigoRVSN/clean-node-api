98130731fb557aa28b63075e72348f3bca7e24f9
rodrigorvsn <rodrigovictor81@gmail.com>
2023-07-02 16:05:46 -0300

    test: remove unused test in survey-routes

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -58,16 +58,5 @@ describe('Login Routes', () => {
         })
         .expect(204)
     })
-
-    it.skip('should return 403 on add survey with invalid accessToken', async () => {
-      await request(app)
-        .post('/api/surveys')
-        .set('x-access-token', '123')
-        .send({
-          question: 'any_question',
-          answers: [{ image: 'any_image', answer: 'any_answer' }, { answer: 'other_answer' }]
-        })
-        .expect(403)
-    })
   })
 })
