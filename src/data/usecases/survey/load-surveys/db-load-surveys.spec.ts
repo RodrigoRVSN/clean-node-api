58120a467b28ad09de5cb4980549b20cb44c1bd4
rodrigorvsn <rodrigovictor81@gmail.com>
2023-07-02 17:49:34 -0300

    feat: ensure DbLoadSurveys throws if Repository throws

--- src/data/usecases/survey/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/survey/load-surveys/db-load-surveys.spec.ts
@@ -51,4 +51,12 @@ describe('DbLoadSurveys', () => {
     const surveys = await sut.load()
     expect(surveys).toEqual(makeFakeSurveys())
   })
+
+  it('should throws if LoadSurveyRepository throws', async () => {
+    const { sut, loadSurveyRepositoryStub } = makeSut()
+
+    jest.spyOn(loadSurveyRepositoryStub, 'loadAll').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow()
+  })
 })
