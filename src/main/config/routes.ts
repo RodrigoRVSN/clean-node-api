c303d259cecd56b9be469c0367dc860f8635582b
rodrigorvsn <rodrigovictor81@gmail.com>
2023-07-04 00:03:19 -0300

    fix: route mapping

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -7,7 +7,7 @@ export const setupRoutes = (app: Express): void => {
   app.use('/api', router)
 
   readdirSync(join(__dirname, '/../routes')).map(async file => {
-    if (!file.includes('.test.')) {
+    if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
