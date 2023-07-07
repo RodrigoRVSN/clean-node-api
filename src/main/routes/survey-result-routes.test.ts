82f8abd2ea1121f70425e0dfb7fe6ab77d69dd09
rodrigorvsn <rodrigovictor81@gmail.com>
2023-07-06 23:06:31 -0300

    feat: ensure save survey result route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -2,6 +2,28 @@ import request from 'supertest'
 import app from '../config/app'
 import { env } from '../config/env'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { type Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+
+let surveyCollection: Collection
+let accountCollection: Collection
+
+const makeAccessToken = async (): Promise<string> => {
+  const { insertedId: id } = await accountCollection.insertOne({
+    name: 'Rodrigo',
+    email: 'rodrigo@mail.com',
+    password: '123',
+    role: 'admin'
+  })
+
+  const accessToken = sign({ id }, env.jwtSecret)
+
+  await accountCollection.updateOne({ _id: id }, {
+    $set: { accessToken }
+  })
+
+  return accessToken
+}
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -12,15 +34,37 @@ describe('Login Routes', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   describe('PUT /surveys/:surveyId/results', () => {
     it('should return 403 on add survey without token', async () => {
       await request(app)
         .put('/api/surveys/any_id/results')
         .send({
-          question: 'any_question',
-          answer: { image: 'any_image', answer: 'any_answer' }
+          answer: 'any_answer'
         })
         .expect(403)
+    })
+
+    it('should return 204 on add survey valid accessToken', async () => {
+      const { insertedId } = await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [{ image: 'any_image', answer: 'Answer 1' }],
+        date: new Date()
+      })
+
+      const accessToken = await makeAccessToken()
+
+      await request(app)
+        .put(`/api/surveys/${insertedId}/results`)
+        .set('x-access-token', accessToken)
+        .send({ answer: 'Answer 1' })
+        .expect(200)
     })
   })
 })
--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result.ts
@@ -17,6 +17,7 @@ export class SaveSurveyResultController implements Controller {
       const answers = survey.answers.map(a => a.answer)
       if (!answers.includes(answer)) return forbidden(new InvalidParamError('answer'))
 
+      console.log({ accountId, surveyId, answer })
       const surveyResult = await this.saveSurveyResult.save({
         accountId: String(accountId),
         surveyId,
